f672b38dec3a009b9423292a28d198d0f9bb2bb5
Kwame Johnson <kwamejohnson@Kwames-MBP-2.lan>
2017-02-01 14:18:40 -0800

    first draft final

--- client/js/codingBucketlistController.js
+++ client/js/codingBucketlistController.js
@@ -3,46 +3,48 @@ app.controller('codingBucketlistController', ['$scope', function($scope){
 console.log("in codingBucketlistController");
 
 $scope.read = [
-                { "title" : "Clean Code by Robert C. Martin","status" : "images/inprog.png"},
-                { "title" : "Programming Pearls by Jon Bentley", "status": "images/future.png"},
-                { "title" : "The Innovators by Walter Isaacson", "status": "images/check3.png"},
-                { "title" : "Cracking the Coding Interview by Gayle Laakmann McDowell" ,"status" : "images/inprog.png"},
-                { "title" : "Soft Skills by John Sonmez","status" : "images/inprog.png"},
-                { "title" : "Zero Bugs and Program Faster by Kate Thompson","status" : "images/future.png"},
-                { "title" : "Code Complete by Steve McConnell","status" : "images/future.png"},
-                { "title" : "The Pragmatic Programmer by Hunt & Thomas","status" : "images/future.png"},
-                { "title" : "Refactoring/xUnit Test Patterns by Gerard Meszaros","status" : "images/future.png"}
+                { "name" : "Clean Code by Robert C. Martin", "status" : "images/inprog.png", "title" : "in progress"},
+                { "name" : "Programming Pearls by Jon Bentley", "status": "images/future.png", "title" : "future" },
+                { "name" : "The Innovators by Walter Isaacson", "status": "images/check3.png", "title" : "completed" },
+                { "name" : "Cracking the Coding Interview by Gayle Laakmann McDowell" ,"status" : "images/inprog.png", "title" : "in progress" },
+                { "name" : "Soft Skills by John Sonmez","status" : "images/inprog.png", "title" : "in progress" },
+                { "name" : "Zero Bugs and Program Faster by Kate Thompson","status" : "images/future.png", "title" : "future" },
+                { "name" : "Code Complete by Steve McConnell","status" : "images/future.png", "title" : "future" },
+                { "name" : "The Pragmatic Programmer by Hunt & Thomas","status" : "images/future.png", "title" : "future" },
+                { "name" : "Refactoring/Unit Test Patterns by Gerard Meszaros","status" : "images/future.png", "title" : "future" }
               ]
 
 $scope.learn = [
-                {"title": "Full-Stack JavaScript", "status": "images/check3.png"},
-                {"title": "Python", "status": "images/check3.png"},
-                {"title": "Swift/iOS Development", "status": "images/check3.png"},
-                {"title": "Ruby Fundamentals", "status": "images/check3.png"},
-                {"title": "C#", "status": "images/check3.png"},
-                {"title": "Perl", "status": "images/future.png"},
-                {"title": "Machine Learning", "status": "images/inprog.png"},
-                {"title": "D3", "status": "images/check3.png"},
+                {"name": "Full-Stack JavaScript", "status": "images/check3.png", "title" : "completed" },
+                {"name": "Python", "status": "images/check3.png", "title" : "completed" },
+                {"name": "Swift/iOS Development", "status": "images/check3.png", "title" : "completed" },
+                {"name": "Ruby", "status": "images/inprog.png", "title" : "in progress" },
+                {"name": "C#", "status": "images/inprog.png", "title" : "in progress" },
+                {"name": "Machine Learning", "status": "images/inprog.png", "title" : "in progress" },
+                {"name": "D3", "status": "images/check3.png", "title" : "completed" },
+                {"name": "Data Science", "status": "images/inprog.png", "title" : "in progress" },
+                {"name": "AWS", "status": "images/inprog.png", "title" : "in progress" }
                 ]
 
 $scope.go = [
-                {"title": "TechCrunch Disrupt", "status": "images/future.png"},
-                {"title": "Apple Worldwide Developers Conference", "status": "images/future.png"},
-                {"title": "Geekwire Summit", "status": "images/future.png"},
-                {"title": "Web Summit", "status": "images/future.png"},
-                {"title": "TED Conference", "status": "images/future.png"},
-                {"title": "Microsoft Build", "status": "images/future.png"},
-                {"title": "WSJ Digital Live", "status": "images/future.png"},
+                {"name": "TechCrunch Disrupt", "status": "images/future.png", "title" : "future" },
+                {"name": "Apple Worldwide Developers Conference", "status": "images/future.png", "title" : "future" },
+                {"name": "Geekwire Summit", "status": "images/future.png", "title" : "future" },
+                {"name": "Web Summit", "status": "images/future.png", "title" : "future" },
+                {"name": "TED Conference", "status": "images/future.png", "title" : "future" },
+                {"name": "TEDMED Conference", "status": "images/future.png", "title" : "future" },
+                {"name": "Microsoft Build", "status": "images/future.png", "title" : "future" },
+                {"name": "WSJ Digital Live", "status": "images/future.png", "title" : "future" },
             ]
 
 $scope.meet = [
-                {"title": "Elon Musk", "status": "images/future.png"},
-                {"title": "Bill Gates", "status": "images/future.png"},
-                {"title": "Satya Nadella", "status": "images/future.png"},
-                {"title": "Tristan Walker", "status": "images/future.png"},
-                {"title": "Jeff Bezos", "status": "images/future.png"},
-                {"title": "Gayle Laakmann McDowell", "status": "images/future.png"},
-                {"title": "Paul Graham", "status": "images/future.png"},
+                {"name": "Elon Musk", "status": "images/future.png", "title" : "future" },
+                {"name": "Bill Gates", "status": "images/future.png", "title" : "future" },
+                {"name": "Satya Nadella", "status": "images/future.png", "title" : "future" },
+                {"name": "Tristan Walker", "status": "images/future.png", "title" : "future" },
+                {"name": "Jeff Bezos", "status": "images/future.png", "title" : "future" },
+                {"name": "Gayle Laakmann McDowell", "status": "images/future.png", "title" : "future" },
+                {"name": "Paul Graham", "status": "images/future.png", "title" : "future" },
               ]
 
 }])
